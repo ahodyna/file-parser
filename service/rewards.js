2016d3b76faeeaeff1b0289a483943c07b8abf28
alina <hodyna.alina@gmail.com>
2024-04-24 11:21:55 +0300

    fix sum of rewards and donation for user

--- service/rewards.js
+++ service/rewards.js
@@ -3,8 +3,8 @@ const RewardsDAO = require('../dao/rewards')
 class FileService {
 	async calculateRewards() {
 		const result = await RewardsDAO.calculateRewards()
-    
-		const users = []
+		const users = [];
+		const userIds = new Set();
 		const totalDonations = result.rows.reduce((acc, e) => {
 			acc += e.total_donation_in_usd;
 			return acc;
@@ -15,7 +15,14 @@ class FileService {
 			const rewardPercentage = (employee.total_donation_in_usd / totalDonations);
 			const reward = Math.round(rewardPercentage * rewardPool);
 
-			users.push({name: employee.name, donated: employee.total_donation_in_usd, reward})
+			if (!userIds.has(employee.id)) {
+				users.push({id: employee.id, name: employee.name, donated: employee.total_donation_in_usd, reward})
+				userIds.add(employee.id)
+			} else {
+				const user = users.find((user) => user.id === employee.id)
+				user.donated += employee.total_donation_in_usd;
+				user.reward += reward;
+			}
 		});
 
 		return users;
